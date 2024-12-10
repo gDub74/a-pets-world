40412e21d3c525bf18d0e8d27c3aac998c3173cc
Greg <gregweber@osgmarketplace.com>
2024-12-09 20:26:20 -0800

    chore: clean up and little refactoring

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,7 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
+/** cn, which is short for the className convention in shadcn, is used to merge className and className from props in a component */
+export const cn = (...inputs: ClassValue[]): string => {
+    return twMerge(clsx(inputs));
+};

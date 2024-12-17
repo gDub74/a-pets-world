1084dfd667c9c98e050335e776ec2087937beb85
Greg <gregweber@osgmarketplace.com>
2024-12-17 14:59:49 -0800

    feat: rework side bar to have different SideBarGroup for social

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -2,9 +2,9 @@
 
 import { ReactElement } from "react";
 import {
-    Home,
+    Newspaper as Feed,
     Inbox,
-    Search,
+    Activity as Social,
     Settings,
     ChartNoAxesColumn as Dashboard,
     HeartHandshake as Adopt,
@@ -29,17 +29,9 @@ import {
 } from "@/components/ui/sidebar";
 import { Badge } from "./ui/badge";
 import { useIsMobile } from "@/hooks/use-mobile";
+import { Separator } from "./ui/separator";
 
-const menuItems = [
-    {
-        title: "Home",
-        url: "#",
-        icon: Home,
-        meta: {
-            description:
-                "This is the home page feed for the user to see the latest updates and posts from the community.",
-        },
-    },
+const publicMenuItems = [
     {
         title: "Adopt",
         url: "#",
@@ -47,35 +39,6 @@ const menuItems = [
         meta: {
             description:
                 "This is the adoption page for the user to see the pets available for adoption via Pet Finder API.",
-        },
-    },
-    {
-        title: "Notifications",
-        url: "#",
-        icon: Notifications,
-        meta: {
-            description:
-                "This is the notifications page for the user to see their notifications.",
-            count: 13, // this will need to be dynamic
-        },
-    },
-    {
-        title: "Messages",
-        url: "#",
-        icon: Inbox,
-        meta: {
-            description:
-                "This is the messages page for the user to see their direct private messages.",
-            count: 1, // this will need to be dynamic
-        },
-    },
-    {
-        title: "Search",
-        url: "#",
-        icon: Search,
-        meta: {
-            description:
-                "This enables the search UI for the user to search for pets, users, and posts.",
         },
     },
     {
@@ -88,24 +51,6 @@ const menuItems = [
         },
     },
     {
-        title: "Profile",
-        url: "#",
-        icon: Profile,
-        meta: {
-            description:
-                "This is the profile page for the user to see their profile information and view and create their posts.",
-        },
-    },
-    {
-        title: "Community",
-        url: "#",
-        icon: Community,
-        meta: {
-            description:
-                "This is the community page for the user to see and comment on community specific posts -- based on location/city posts and updates.",
-        },
-    },
-    {
         title: "Marketplace",
         url: "#",
         icon: Marketplace,
@@ -115,6 +60,66 @@ const menuItems = [
         },
     },
 ];
+
+const socialAccountItems = {
+    title: "Social",
+    url: "#",
+    icon: Social,
+    meta: {
+        description:
+            "This is the parent for all social media pages. Social Media requires a user to be signed in.",
+    },
+    children: [
+        {
+            title: "Profile",
+            url: "#",
+            icon: Profile,
+            meta: {
+                description:
+                    "This is the profile page for the user to see their profile information and view and create their posts.",
+            },
+        },
+        {
+            title: "Feed",
+            url: "#",
+            icon: Feed,
+            meta: {
+                description:
+                    "This is the home page feed for the user to see the latest updates and posts from the community.",
+            },
+        },
+        {
+            title: "Community",
+            url: "#",
+            icon: Community,
+            meta: {
+                description:
+                    "This is the community feed for the user to see and comment on community specific posts -- based on location/city posts and updates.",
+            },
+        },
+        {
+            title: "Notifications",
+            url: "#",
+            icon: Notifications,
+            meta: {
+                description:
+                    "This is the notifications page for the user to see their notifications.",
+                count: 13, // this will need to be dynamic
+            },
+        },
+
+        {
+            title: "Messages",
+            url: "#",
+            icon: Inbox,
+            meta: {
+                description:
+                    "This is the messages page for the user to see their direct private messages.",
+                count: 1, // this will need to be dynamic
+            },
+        },
+    ],
+};
 
 const footerItems = [
     {
@@ -135,14 +140,41 @@ export const AppSidebar = (): ReactElement => {
     return (
         <Sidebar collapsible={`${isMobile ? "offcanvas" : "icon"}`}>
             <SidebarContent className="h-full p-3">
-                <SidebarGroup className="h-full">
-                    <SidebarGroupLabel className="h-16 text-base font-bold mb-12">
-                        {/* TODO: Replace with svg logo */}
+                <SidebarGroup>
+                    <SidebarGroupLabel className="h-16 text-base font-bold mb-8">
                         {"A Pet's World"}
                     </SidebarGroupLabel>
                     <SidebarGroupContent>
+                        <SidebarMenu className="gap-6 mb-8">
+                            {publicMenuItems.map((item) => (
+                                <SidebarMenuItem key={item.title}>
+                                    <SidebarMenuButton
+                                        asChild
+                                        className="[&_svg]:size-6"
+                                    >
+                                        <a href={item.url}>
+                                            <item.icon className="mr-2" />
+                                            <span className="text-base my-12 font-semibold">
+                                                {item.title}
+                                            </span>
+                                        </a>
+                                    </SidebarMenuButton>
+                                </SidebarMenuItem>
+                            ))}
+                        </SidebarMenu>
+                    </SidebarGroupContent>
+                </SidebarGroup>
+                <Separator className="my-4" />
+                <SidebarGroup>
+                    <SidebarGroupLabel className="h-12 text-sm font-bold mb-4 ">
+                        {socialAccountItems.title}
+                        <span className="ml-2">
+                            <socialAccountItems.icon size="20" />
+                        </span>
+                    </SidebarGroupLabel>
+                    <SidebarGroupContent>
                         <SidebarMenu className="gap-6">
-                            {menuItems.map((item) => (
+                            {socialAccountItems.children.map((item) => (
                                 <SidebarMenuItem key={item.title}>
                                     <SidebarMenuButton
                                         asChild

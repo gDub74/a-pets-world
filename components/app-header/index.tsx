22091c17e2f11d99d3be89b03e58059fe30c8fc6
Greg <gregweber@osgmarketplace.com>
2024-12-18 17:42:21 -0800

    fix: add avatar signin button

--- components/app-header/index.tsx
+++ components/app-header/index.tsx
@@ -1,40 +1,22 @@
 "use client";
 
-import { ReactElement, useState } from "react";
+import { ReactElement } from "react";
 import { ModeToggleButton } from "./mode-toggle-button";
 import { SidebarTrigger } from "../ui/sidebar";
-import { RedirectToSignIn, SignedOut, UserButton } from "@clerk/nextjs";
-import { CircleUser } from "lucide-react";
-import { Button } from "../ui/button";
+import { UserButton } from "@clerk/nextjs";
+import { AvatarSignInButton } from "./avatar-sign-in-button";
 
-export const AppHeader = (): ReactElement => {
-    const [redirectToSignIn, setRedirectToSignIn] = useState(false);
-
-    if (redirectToSignIn) {
-        return <RedirectToSignIn />;
-    }
-
-    return (
-        <header className="sticky top-0 z-50 w-full bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 dark:border-border">
-            <div className="sticky top-0 z-50 w-full flex items-center">
-                <SidebarTrigger />
-                <div className="ml-auto flex items-center">
-                    <ModeToggleButton className="mr-2" />
-                    <span className="mt-1 mr-1">
-                        <UserButton />
-                    </span>
-                    <SignedOut>
-                        <Button
-                            onClick={() => setRedirectToSignIn(true)}
-                            variant="ghost"
-                            className="w-7 h-7 bg-transparent [&_svg]:size-5"
-                        >
-                            <CircleUser color={"hsl(var(--primary))"} />
-                            <span className="sr-only">Sign in</span>
-                        </Button>
-                    </SignedOut>
-                </div>
+export const AppHeader = (): ReactElement => (
+    <header className="sticky top-0 z-50 w-full bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 dark:border-border">
+        <div className="sticky top-0 z-50 w-full flex items-center">
+            <SidebarTrigger />
+            <div className="ml-auto flex items-center">
+                <ModeToggleButton className="mr-2" />
+                <span className="mt-1 mr-1">
+                    <UserButton />
+                </span>
+                <AvatarSignInButton />
             </div>
-        </header>
-    );
-};
+        </div>
+    </header>
+);

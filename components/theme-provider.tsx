1a346b9ddea4d1616399b7aa9a211cdade6a50db
Greg <gregweber@osgmarketplace.com>
2025-01-03 16:15:09 -0800

    feat: upgrade to next 15, react 19 and node 20

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,11 +1,24 @@
 "use client";
 
-import { ComponentProps } from "react";
+import { ComponentProps, useEffect, useState } from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 
 export function ThemeProvider({
     children,
     ...props
 }: ComponentProps<typeof NextThemesProvider>) {
+    /**
+     *  This silliness is a suggested solution to prevent Hydration issue since the theme is not available on the server.
+     *  https://nextjs.org/docs/messages/react-hydration-error#solution-1-using-useeffect-to-run-on-the-client-only
+     * **/
+    const [isClient, setIsClient] = useState(false);
+    useEffect(() => {
+        setIsClient(true);
+    }, []);
+
+    if (!isClient) {
+        return null;
+    }
+
     return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
